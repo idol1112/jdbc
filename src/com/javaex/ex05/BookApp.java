aaf8a01b118b1cb6f0a2a7a0a0e8caf85e1d99b4
hi <hi@DESKTOP-OAV2V8U>
2021-07-02 17:39:26 +0900

    07/02 BookDao+검색 만들기 과제

---
+++ src/com/javaex/ex05/BookApp.java
@@ -0,0 +1,33 @@
+package com.javaex.ex05;
+
+public class BookApp {
+
+	public static void main(String[] args) {
+		
+		//(1)
+		//작가테이블 책테이블 완성
+		//작가테이블 시퀀스, 책테이블 시퀀스 완성
+		
+		//authorDao.authorInsert(); 이용해서 작가 6명추가
+		
+		//bookDao.bookInsert(); 책8개 추가
+		
+		//책수정, 착제 각자 알아서 테스트
+		
+		//책리스트 출력
+		//bookDao.getBookList(); --> 8개
+		////////////////////////////////////////////////////
+		
+		
+		//(2)
+		//스캐너를 통해서 사용자한테 키워드 입력받음
+		//시작할때 화면
+		//"검색어를 입력해주세요"
+		//"검색어: 문
+		
+		//bookDao.getBookList(keyword);  -->4개
+		//검색된 책정보 출력
+
+	}
+
+}
---
+++ src/com/javaex/ex05/BookVo.java
@@ -0,0 +1,13 @@
+package com.javaex.ex05;
+
+public class BookVo {
+	
+	private int bookId;
+	private String title;
+	private String pubs;
+	private String pub_date;
+	private int authorId;
+	private String authorName;
+	private String authorDesc;
+
+}
