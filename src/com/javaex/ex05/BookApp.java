8d718b8a080089a5a425475ab1511ae2e70bc0ef
idol1112 <idol981112@gmail.com>
2021-07-05 13:26:27 +0900

    07/05 BookDao,vo

--- src/com/javaex/ex05/BookApp.java
+++ src/com/javaex/ex05/BookApp.java
@@ -1,32 +1,137 @@
 package com.javaex.ex05;
 
+import java.util.List;
+import java.util.Scanner;
+
 public class BookApp {
 
 	public static void main(String[] args) {
-		
-		//(1)
-		//작가테이블 책테이블 완성
-		//작가테이블 시퀀스, 책테이블 시퀀스 완성
-		
-		//authorDao.authorInsert(); 이용해서 작가 6명추가
-		
-		//bookDao.bookInsert(); 책8개 추가
-		
-		//책수정, 착제 각자 알아서 테스트
-		
-		//책리스트 출력
-		//bookDao.getBookList(); --> 8개
-		////////////////////////////////////////////////////
-		
-		
-		//(2)
-		//스캐너를 통해서 사용자한테 키워드 입력받음
-		//시작할때 화면
-		//"검색어를 입력해주세요"
-		//"검색어: 문
-		
-		//bookDao.getBookList(keyword);  -->4개
-		//검색된 책정보 출력
+
+		Scanner sc = new Scanner(System.in);
+
+		AuthorDao authorDao = new AuthorDao();
+		List<AuthorVo> authorList;
+
+		BookDao bookDao = new BookDao();
+		List<BookVo> bookList;
+		List<BookVo> bookSearchList;
+
+		// 작가테이블 책테이블 완성 --> 내용은 안 넣기
+		// 작가테이블 시퀀스, 책테이블 시퀀스 완성
+		authorDao.authorTable();
+		authorDao.authorSeq();
+
+		bookDao.bookTable();
+		bookDao.bookSeq();
+
+		// authorDao.authorInsert(); 이용해서 데이터 추가 --> 여기서 내용 추가, 작가 6명
+		AuthorVo iAuthorVo_01 = new AuthorVo("이문열", "경북 영양");
+		authorDao.authorInsert(iAuthorVo_01);
+
+		AuthorVo iAuthorVo_02 = new AuthorVo("박경리", "경상남도 통영");
+		authorDao.authorInsert(iAuthorVo_02);
+
+		AuthorVo iAuthorVo_03 = new AuthorVo("이고잉", "가명");
+		authorDao.authorInsert(iAuthorVo_03);
+
+		AuthorVo iAuthorVo_04 = new AuthorVo("기안84", "기안동에서 산 84년생");
+		authorDao.authorInsert(iAuthorVo_04);
+
+		AuthorVo iAuthorVo_05 = new AuthorVo("강풀", "온라인 만화가 1세대");
+		authorDao.authorInsert(iAuthorVo_05);
+
+		AuthorVo iAuthorVo_06 = new AuthorVo("김영하", "알쓸신잡");
+		authorDao.authorInsert(iAuthorVo_06);
+
+		// bookDao.bookInsert(); 책 8개 추가
+		BookVo iBookVo_01 = new BookVo("우리들의 일그러진 영웅", "다림", "1998-02-22", 1);
+		bookDao.bookInsert(iBookVo_01);
+
+		BookVo iBookVo_02 = new BookVo("삼국지", "민음사", "2002-03-01", 1);
+		bookDao.bookInsert(iBookVo_02);
+
+		BookVo iBookVo_03 = new BookVo("토지", "마로니에북스", "2012-08-15", 2);
+		bookDao.bookInsert(iBookVo_03);
+
+		BookVo iBookVo_04 = new BookVo("자바프로그래밍 입문", "위키북스", "2015-04-01", 3);
+		bookDao.bookInsert(iBookVo_04);
+
+		BookVo iBookVo_05 = new BookVo("패션왕", "중앙북스(books)", "2012-02-22", 4);
+		bookDao.bookInsert(iBookVo_05);
+
+		BookVo iBookVo_06 = new BookVo("순정만화", "재미주의", "2011-08-03", 5);
+		bookDao.bookInsert(iBookVo_06);
+
+		BookVo iBookVo_07 = new BookVo("오직두사람", "문학동네", "2017-05-04", 6);
+		bookDao.bookInsert(iBookVo_07);
+
+		BookVo iBookVo_08 = new BookVo("26년", "재미주의", "2012-02-04", 5);
+		bookDao.bookInsert(iBookVo_08);
+
+		// 리스트출력
+		// DB에서 가져오기
+		bookList = bookDao.getBookList();
+		// 리스트를 for문으로 출력 --> 메소드로 정의
+		printList(bookList);
+
+		/*****************************
+		 * 수정&삭제 테스트 //수정 테스트 AuthorVo uAuthorVo = new AuthorVo(7, "김덕배", "50만 유튜버");
+		 * authorDao.authorUpdate(uAuthorVo);
+		 * 
+		 * BookVo uBookVo = new BookVo("삶의 현장 르포", "이순재", "2021-05-05", 4, 5);
+		 * bookDao.bookUpdate(uBookVo);
+		 * 
+		 * //책리스트 출력 //bookDao.getBookList(); --> 8개
+		 * 
+		 * //리스트출력 //DB에서 가져오기 bookList = bookDao.getBookList(); //리스트를 for문으로 출력 -->
+		 * 메소드로 정의 printList(bookList);
+		 * 
+		 * 
+		 * 
+		 * 
+		 * //삭제 테스트 int dCount = authorDao.authorDelete(1);
+		 * 
+		 * //DB에서 가져오기 bookList = bookDao.getBookList(); //리스트를 for문으로 출력 --> 메소드로 정의
+		 * printList(bookList);
+		 * 
+		 ********************************/
+
+		/*************** 위에 다 한 사람 추가로 하기 **************/
+		// 스캐너를 통해서 사용자한테 키워드 입력받음
+		// "검색을 입력해주세요"
+		// "검색어: 문"
+
+		// bookDao.getBookList(keyword); --> 4개
+		// 책 정보 출력(문이 들어간)
+		System.out.println("검색을 입력해주세요");
+		System.out.print("검색어: ");
+		String keyword = sc.nextLine();
+
+		bookSearchList = bookDao.getBookList(keyword);
+		printList(bookSearchList);
+
+		/*
+		 * BookDao의 메소드에서 스캐너 사용하는 방법 bookSearchList = bookDao.bookSearch(); //리스트를
+		 * for문으로 출력 --> 메소드로 정의 printList(bookSearchList);
+		 */
+
+		sc.close();
+
+	}
+
+	public static void printList(List<BookVo> bookList) {
+
+		for (int i = 0; i < bookList.size(); i++) {
+
+			BookVo bookVo = bookList.get(i);
+			System.out.println(bookVo.getBookId() + "\t" + bookVo.getTitle() + "\t" + bookVo.getPubs() + "\t"
+					+ bookVo.getPubDate() + "\t" + bookVo.getAuthorId() + "\t" + bookVo.getAuthorName() + "\t"
+					+ bookVo.getAuthorDesc());
+
+		}
+
+		System.out.println("===============================");
+		System.out.println("");
 
 	}
 
--- src/com/javaex/ex05/BookDao.java
+++ src/com/javaex/ex05/BookDao.java
@@ -1,5 +1,382 @@
 package com.javaex.ex05;
 
-public class BookDao {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+public class BookDao {
+	// 필드
+	private Connection conn = null;
+	private PreparedStatement pstmt = null;
+	private ResultSet rs = null;
+
+	private String driver = "oracle.jdbc.driver.OracleDriver";
+	private String url = "jdbc:oracle:thin:@localhost:1521:xe";
+	private String id = "webdb";
+	private String pw = "webdb";
+
+	// 생성자
+
+	// 메소드-gs
+
+	// 메소드-일반
+
+	// DB연결
+	public void getConnection() {
+
+		try {
+
+			// 1. JDBC 드라이버 (Oracle) 로딩
+			Class.forName(driver);
+
+			// 2. Connection 얻어오기
+			conn = DriverManager.getConnection(url, id, pw);
+
+		} catch (ClassNotFoundException e) {
+			System.out.println("error: 드라이버 로딩 실패 - " + e);
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		}
+
+	}
+
+	// 자원정리
+	public void close() {
+		// 5. 자원정리
+		try {
+			if (rs != null) {
+				rs.close();
+			}
+			if (pstmt != null) {
+				pstmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		}
+	}
+
+	// 북 테이블 만들기
+	public void bookTable() {
+
+		this.getConnection();
+
+		try {
+
+			// 3. SQL문 준비 / 바인딩 / 실행
+			String query = "";
+			query += " create table book( ";
+			query += "     book_id number(5), ";
+			query += "     title varchar2(50), ";
+			query += "     pubs varchar2(50), ";
+			query += "     pub_date date, ";
+			query += "     author_id number(5), ";
+			query += "     primary key(book_id), ";
+			query += "     constraint book_fk foreign key(author_id)  ";
+			query += "     references author(author_id) ";
+			query += "	   ON DELETE CASCADE ";
+			query += " ) ";
+
+			pstmt = conn.prepareStatement(query);
+
+			pstmt.executeUpdate();
+
+			// 4.결과처리
+			System.out.println("북테이블 생성");
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		this.close();
+
+	}
+
+	// 시퀀스 생성
+	public void bookSeq() {
+
+		this.getConnection();
+
+		try {
+
+			// 3. SQL문 준비 / 바인딩 / 실행
+			String query = "";
+			query += " create sequence seq_book_id ";
+			query += " increment by 1 ";
+			query += " start with 1 ";
+			query += " nocache ";
+
+			pstmt = conn.prepareStatement(query);
+
+			// 4.결과처리
+			System.out.println("북테이블 시퀀스 생성");
+
+			pstmt.executeUpdate();
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		this.close();
+	}
+
+	// 책 삭제하기
+	public int bookDelete(int bookId) {
+
+		int count = -1;
+
+		this.getConnection();
+
+		try {
+			// 3. SQL문 준비 / 바인딩 / 실행
+			String query = "";
+			query += " delete from book ";
+			query += " where book_id = ? ";
+
+			pstmt = conn.prepareStatement(query);
+			pstmt.setInt(1, bookId);
+
+			count = pstmt.executeUpdate();
+
+			// 4.결과처리
+			System.out.println(count + "건 삭제");
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		this.close();
+
+		return count;
+	}
+
+	// 북수정
+	public int bookUpdate(BookVo bookVo) {
+
+		int count = -1;
+
+		this.getConnection();
+
+		try {
+			// 3. SQL문 준비 / 바인딩 / 실행
+			String query = "";
+			query += " update book ";
+			query += " set title = ?, ";
+			query += "     pubs = ?, ";
+			query += "     pub_date = ? , ";
+			query += "     author_id = ? ";
+			query += " where book_id = ? ";
+
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, bookVo.getTitle());
+			pstmt.setString(2, bookVo.getPubs());
+			pstmt.setString(3, bookVo.getPubDate());
+			pstmt.setInt(4, bookVo.getAuthorId());
+			pstmt.setInt(5, bookVo.getBookId());
+
+			count = pstmt.executeUpdate();
+
+			// 4.결과처리
+			System.out.println(count + "건 수정");
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		this.close();
+
+		return count;
+
+	}
+
+	// 북 등록
+	public int bookInsert(BookVo bookVo) {
+
+		int count = -1;
+
+		this.getConnection();
+
+		try {
+
+			// 3. SQL문 준비 / 바인딩 / 실행
+			String query = "";
+			query += " insert into book ";
+			query += " values(seq_book_id.nextval, ?, ?, ?, ?) ";
+
+			pstmt = conn.prepareStatement(query);
+			pstmt.setString(1, bookVo.getTitle());
+			pstmt.setString(2, bookVo.getPubs());
+			pstmt.setString(3, bookVo.getPubDate());
+			pstmt.setInt(4, bookVo.getAuthorId());
+
+			count = pstmt.executeUpdate();
+
+			// 4.결과처리
+			System.out.println(count + "건 등록");
+
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		this.close();
+
+		return count;
+
+	}
+
+	// 북 리스트 가져오기
+	public List<BookVo> getBookList() {
+
+		List<BookVo> bookList = new ArrayList<BookVo>();
+
+		this.getConnection();
+
+		try {
+
+			// 3. SQL문 준비 / 바인딩 / 실행
+			String query = "";
+			query += " select  b.book_id, ";
+			query += "         b.title, ";
+			query += "         b.pubs, ";
+			query += "         to_char(b.pub_date, 'yyyy-mm-dd') pubDate, ";
+			query += "         a.author_id, ";
+			query += "         a.author_name, ";
+			query += "         a.author_desc ";
+			query += " from book b, author a ";
+			query += " where b.author_id = a.author_id ";
+
+			pstmt = conn.prepareStatement(query);
+			rs = pstmt.executeQuery();
+
+			// 4.결과처리
+			while (rs.next()) {
+				int bookId = rs.getInt("book_id");
+				String bookTitle = rs.getString("title");
+				String bookPubs = rs.getString("pubs");
+				String bookPubdate = rs.getString("pubDate");
+				int authorId = rs.getInt("author_id");
+				String authorName = rs.getString("author_name");
+				String authorDesc = rs.getString("author_desc");
+
+				BookVo bookVo = new BookVo(bookId, bookTitle, bookPubs, bookPubdate, authorId, authorName, authorDesc);
+
+				bookList.add(bookVo);
+
+			}
+
+		} catch (SQLException e) {
+			System.out.println("error:" + e);
+		}
+
+		this.close();
+
+		return bookList;
+
+	}
+
+	/*
+	 * BookDao의 메소드에서 스캐너 사용하는 방법 //검색하기 public List<BookVo> bookSearch() {
+	 * 
+	 * List<BookVo> bookSearch = new ArrayList<BookVo>();
+	 * 
+	 * Scanner sc = new Scanner(System.in); this.getConnection();
+	 * 
+	 * try {
+	 * 
+	 * System.out.println("검색을 입력해주세요"); System.out.print("검색어: "); String search =
+	 * sc.nextLine();
+	 * 
+	 * // 3. SQL문 준비 / 바인딩 / 실행 String query = ""; query += " select  b.book_id, ";
+	 * query += "         b.title, "; query += "         b.pubs, "; query +=
+	 * "         to_char(b.pub_date, 'yyyy-mm-dd') pubDate, "; query +=
+	 * "         a.author_id, "; query += "         a.author_name, "; query +=
+	 * "         a.author_desc "; query += " from book b, author a "; query +=
+	 * " where b.author_id = a.author_id "; query +=
+	 * " and (b.pubs || b.title || a.author_name) like ";
+	 * 
+	 * pstmt = conn.prepareStatement(query + "'%" + search + "%'");
+	 * 
+	 * pstmt.executeUpdate();
+	 * 
+	 * rs = pstmt.executeQuery();
+	 * 
+	 * // 4.결과처리 while(rs.next()) { int bookId = rs.getInt("book_id"); String
+	 * bookTitle = rs.getString("title"); String bookPubs = rs.getString("pubs");
+	 * String bookPubdate = rs.getString("pubDate"); int authorId =
+	 * rs.getInt("author_id"); String authorName = rs.getString("author_name");
+	 * String authorDesc = rs.getString("author_desc");
+	 * 
+	 * BookVo bookVo = new BookVo(bookId, bookTitle, bookPubs, bookPubdate,
+	 * authorId, authorName, authorDesc);
+	 * 
+	 * bookSearch.add(bookVo);
+	 * 
+	 * }
+	 * 
+	 * } catch (SQLException e) { e.printStackTrace(); }
+	 * 
+	 * sc.close(); this.close();
+	 * 
+	 * return bookSearch; }
+	 */
+
+	// 검색하기
+	public List<BookVo> getBookList(String keyword) {
+				
+				List<BookVo> bookSearchList = new ArrayList<BookVo>();
+
+				this.getConnection();
+				
+				try {
+					
+					// 3. SQL문 준비 / 바인딩 / 실행
+					String query = "";
+					query += " select  b.book_id, ";
+					query += "         b.title, ";
+					query += "         b.pubs, ";
+					query += "         to_char(b.pub_date, 'yyyy-mm-dd') pubDate, ";
+					query += "         a.author_id, ";
+					query += "         a.author_name, ";
+					query += "         a.author_desc ";
+					query += " from book b, author a ";
+					query += " where b.author_id = a.author_id ";
+					query += " and (b.pubs || b.title || a.author_name) like ";
+					
+					pstmt = conn.prepareStatement(query + "'%" + keyword + "%'");
+					
+					pstmt.executeUpdate();
+					
+					rs = pstmt.executeQuery();
+					
+					// 4.결과처리
+					while(rs.next()) {
+						int bookId = rs.getInt("book_id");
+						String bookTitle = rs.getString("title");
+						String bookPubs = rs.getString("pubs");
+						String bookPubdate = rs.getString("pubDate");
+						int authorId = rs.getInt("author_id");
+						String authorName = rs.getString("author_name");
+						String authorDesc = rs.getString("author_desc");
+						
+						BookVo bookVo = new BookVo(bookId, bookTitle, bookPubs, bookPubdate, authorId, authorName, authorDesc);
+						
+						bookSearchList.add(bookVo);
+						
+					}		
+					
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+				
+				this.close();
+				
+				return bookSearchList;
+
+	}
 }
